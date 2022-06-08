7414da44e3717f5141ffc47fcd20ad5858cd5180
febomtempo <febomtempo@hotmail.com>
2022-06-08 01:16:15 -0300

    correções no controller, atualização adonis

--- app/Controllers/Http/ClientesController.ts
+++ app/Controllers/Http/ClientesController.ts
@@ -1,146 +1,150 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Cliente from 'App/Models/Cliente'
-import {schema, rules} from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
+//import Mail from '@ioc:Adonis/Addons/Mail'
 
 export default class ClientesController {
+  public async store({ request, response }: HttpContextContract) {
+    const validationSchema = schema.create({
+      nome: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(3),
+        rules.maxLength(60),
+      ]),
+      sobrenome: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(3),
+        rules.maxLength(60),
+      ]),
+      fone: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(10),
+        rules.maxLength(20),
+      ]),
+      email: schema.string({ trim: true }, [
+        rules.required(),
+        rules.unique({ table: 'usuarios', column: 'email' }),
+        rules.unique({ table: 'clientes', column: 'email' }),
+        rules.email(),
+      ]),
+      password: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(6),
+        rules.maxLength(64),
+      ]),
+    })
 
-    public async store({ request, response }: HttpContextContract) {
-
-      const validationSchema = schema.create({
-        nome: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(3),
-          rules.maxLength(60)]),
-        sobrenome: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(3),
-          rules.maxLength(60)]),
-        fone: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(10),
-          rules.maxLength(20)]),
-        email: schema.string({trim: true}, [
-          rules.required(),
-          rules.unique({table: 'usuarios', column: 'email'}),
-          rules.unique({table: 'clientes', column: 'email'}),
-          rules.email]),
-        password: schema.string({trim: true}, [
-            rules.required(),
-            rules.minLength(6),
-            rules.maxLength(64)]),
-      })
-
-      const messages = {
-        minLength: '{{ field }} deve ter no mínimo {{ options.minLength }} caracteres',
-        maxLength: '{{ field }} deve ter no máximo {{ options.maxLength }} caracteres',
-        required: '{{ field }} é obrigatório',
-        unique: '{{ field }} deve ser único, esse {{ field }} já foi utilizado',
-        regex: '{{ field}} deve seguir o padrão: xxx.xxx.xxx-xx onde x = número',
-        number:'{{ field }} deve conter um número',
-        exists: '{{ field }} deve existir na tabela {{ options.table }}'
-      }
-
-
-      const body = await request.validate({schema: validationSchema, messages})
-        
-    
-    
-      const cliente = await Cliente.create(body)
-    
-        response.status(201)
-
-    
-        return {
-          message: 'Cliente registrado com sucesso!',
-          data: cliente,
-        }
-    }
-      
-
-    public async index(){
-        const cliente = await Cliente.all()
-        
-        return{
-          data:cliente,
-        }
-    }
-    
-    public async show({params}: HttpContextContract){
-        const cliente = await Cliente.findOrFail(params.id)
-    
-        
-    
-        return{
-          data:cliente,
-        }
+    const messages = {
+      minLength: '{{ field }} deve ter no mínimo {{ options.minLength }} caracteres',
+      maxLength: '{{ field }} deve ter no máximo {{ options.maxLength }} caracteres',
+      required: '{{ field }} é obrigatório',
+      unique: '{{ field }} deve ser único, esse {{ field }} já foi utilizado',
+      regex: '{{ field}} deve seguir o padrão: xxx.xxx.xxx-xx onde x = número',
+      number: '{{ field }} deve conter um número',
+      exists: '{{ field }} deve existir na tabela {{ options.table }}',
     }
 
-    public async destroy({params}: HttpContextContract){
-        const cliente = await Cliente.findOrFail(params.id)
-        await cliente.delete()
+    const body = await request.validate({ schema: validationSchema, messages })
 
-        
-        return{
-          message: 'Cliente Deletado com sucesso!',
-          data:cliente,
-        }
+    const cliente = await Cliente.create(body)
+
+    response.status(201)
+
+    /*await Mail.send((message) => {
+      message
+        .from('rajfood2022@hotmail.com', 'RajFood')
+        .to(cliente.email)
+        .subject('Seja bem- vindo!')
+        .text('Seu ID é: ' + cliente.id.toString())
+    })*/
+
+    return {
+      message: 'Cliente registrado com sucesso!',
+      data: cliente,
     }
-    
-    public async update({params, request}: HttpContextContract){
+  }
 
-      const validationSchema = schema.create({
-        nome: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(3),
-          rules.maxLength(60)]),
-        sobrenome: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(3),
-          rules.maxLength(60)]),
-        fone: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(10),
-          rules.maxLength(20)]),
-        email: schema.string({trim: true}, [
-          rules.required(),
-          rules.unique({table: 'usuarios', column: 'email'}),
-          rules.unique({table: 'clientes', column: 'email'}),
-          rules.email]),
-        password: schema.string({trim: true}, [
-            rules.required(),
-            rules.minLength(6),
-            rules.maxLength(64)]),
-      })
+  public async index() {
+    const cliente = await Cliente.all()
 
-      const messages = {
-        minLength: '{{ field }} deve ter no mínimo {{ options.minLength }} caracteres',
-        maxLength: '{{ field }} deve ter no máximo {{ options.maxLength }} caracteres',
-        required: '{{ field }} é obrigatório',
-        unique: '{{ field }} deve ser único, esse {{ field }} já foi utilizado',
-        regex: '{{ field}} deve seguir o padrão: xxx.xxx.xxx-xx onde x = número',
-        number:'{{ field }} deve conter um número',
-        exists: '{{ field }} deve existir na tabela {{ options.table }}'
-      }
-
-
-      const body = await request.validate({schema: validationSchema, messages})
-       
-        const cliente = await Cliente.findOrFail(params.id)
-    
-        cliente.nome = body.nome
-        cliente.sobrenome = body.sobrenome
-        cliente.fone = body.fone
-        cliente.email = body.email
-        cliente.password = body.password
-    
-        
-        await cliente.save()
-
-        
-    
-        return{
-          message: 'Cliente Atualizado com Sucesso!',
-          data: cliente,
-        }
+    return {
+      data: cliente,
     }
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const cliente = await Cliente.findOrFail(params.id)
+
+    return {
+      data: cliente,
+    }
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const cliente = await Cliente.findOrFail(params.id)
+    await cliente.delete()
+
+    return {
+      message: 'Cliente Deletado com sucesso!',
+      data: cliente,
+    }
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const validationSchema = schema.create({
+      nome: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(3),
+        rules.maxLength(60),
+      ]),
+      sobrenome: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(3),
+        rules.maxLength(60),
+      ]),
+      fone: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(10),
+        rules.maxLength(20),
+      ]),
+      email: schema.string({ trim: true }, [
+        rules.required(),
+        rules.unique({ table: 'usuarios', column: 'email' }),
+        rules.unique({ table: 'clientes', column: 'email' }),
+        rules.email(),
+      ]),
+      password: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(6),
+        rules.maxLength(64),
+      ]),
+    })
+
+    const messages = {
+      minLength: '{{ field }} deve ter no mínimo {{ options.minLength }} caracteres',
+      maxLength: '{{ field }} deve ter no máximo {{ options.maxLength }} caracteres',
+      required: '{{ field }} é obrigatório',
+      unique: '{{ field }} deve ser único, esse {{ field }} já foi utilizado',
+      regex: '{{ field}} deve seguir o padrão: xxx.xxx.xxx-xx onde x = número',
+      number: '{{ field }} deve conter um número',
+      exists: '{{ field }} deve existir na tabela {{ options.table }}',
+    }
+
+    const body = await request.validate({ schema: validationSchema, messages })
+
+    const cliente = await Cliente.findOrFail(params.id)
+
+    cliente.nome = body.nome
+    cliente.sobrenome = body.sobrenome
+    cliente.fone = body.fone
+    cliente.email = body.email
+    cliente.password = body.password
+
+    await cliente.save()
+
+    return {
+      message: 'Cliente Atualizado com Sucesso!',
+      data: cliente,
+    }
+  }
 }
--- app/Controllers/Http/UsuariosController.ts
+++ app/Controllers/Http/UsuariosController.ts
@@ -1,144 +1,141 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Usuario from 'App/Models/Usuario'
-import {schema, rules} from '@ioc:Adonis/Core/Validator'
+import { schema, rules } from '@ioc:Adonis/Core/Validator'
 
 export default class UsuariosController {
+  public async store({ request, response }: HttpContextContract) {
+    const validationSchema = schema.create({
+      nome: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(3),
+        rules.maxLength(60),
+      ]),
+      sobrenome: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(3),
+        rules.maxLength(60),
+      ]),
+      fone: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(10),
+        rules.maxLength(20),
+      ]),
+      email: schema.string({ trim: true }, [
+        rules.required(),
+        rules.unique({ table: 'usuarios', column: 'email' }),
+        rules.unique({ table: 'clientes', column: 'email' }),
+        rules.email(),
+      ]),
+      password: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(6),
+        rules.maxLength(64),
+      ]),
+    })
 
-    public async store({ request, response }: HttpContextContract) {
-
-      const validationSchema = schema.create({
-        nome: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(3),
-          rules.maxLength(60)]),
-        sobrenome: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(3),
-          rules.maxLength(60)]),
-        fone: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(10),
-          rules.maxLength(20)]),
-        email: schema.string({trim: true}, [
-          rules.required(),
-          rules.unique({table: 'usuarios', column: 'email'}),
-          rules.unique({table: 'clientes', column: 'email'}),
-          rules.email]),
-        password: schema.string({trim: true}, [
-            rules.required(),
-            rules.minLength(6),
-            rules.maxLength(64)]),
-      })
-
-      const messages = {
-        minLength: '{{ field }} deve ter no mínimo {{ options.minLength }} caracteres',
-        maxLength: '{{ field }} deve ter no máximo {{ options.maxLength }} caracteres',
-        required: '{{ field }} é obrigatório',
-        unique: '{{ field }} deve ser único, esse {{ field }} já foi utilizado',
-        regex: '{{ field}} deve seguir o padrão: xxx.xxx.xxx-xx onde x = número',
-        number:'{{ field }} deve conter um número',
-        exists: '{{ field }} deve existir na tabela {{ options.table }}'
-      }
-
-
-      const body = await request.validate({schema: validationSchema, messages})
-    
-        const usuario = await Usuario.create(body)
-    
-        response.status(201)
-
-    
-        return {
-          message: 'usuario registrado com sucesso!',
-          data: usuario,
-        }
-    }
-      
-
-    public async index(){
-        const usuario = await Usuario.all()
-        
-        return{
-          data:usuario,
-        }
-    }
-    
-    public async show({params}: HttpContextContract){
-        const usuario = await Usuario.findOrFail(params.id)
-    
-        
-    
-        return{
-          data:usuario,
-        }
+    const messages = {
+      minLength: '{{ field }} deve ter no mínimo {{ options.minLength }} caracteres',
+      maxLength: '{{ field }} deve ter no máximo {{ options.maxLength }} caracteres',
+      required: '{{ field }} é obrigatório',
+      unique: '{{ field }} deve ser único, esse {{ field }} já foi utilizado',
+      regex: '{{ field}} deve seguir o padrão: xxx.xxx.xxx-xx onde x = número',
+      number: '{{ field }} deve conter um número',
+      exists: '{{ field }} deve existir na tabela {{ options.table }}',
     }
 
-    public async destroy({params}: HttpContextContract){
-        const usuario = await Usuario.findOrFail(params.id)
-        await usuario.delete()
+    const body = await request.validate({ schema: validationSchema, messages })
 
-        
-        return{
-          message: 'usuario Deletado com sucesso!',
-          data:usuario,
-        }
+    const usuario = await Usuario.create(body)
+
+    response.status(201)
+
+    return {
+      message: 'usuario registrado com sucesso!',
+      data: usuario,
     }
-    
-    public async update({params, request}: HttpContextContract){
+  }
 
-      const validationSchema = schema.create({
-        nome: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(3),
-          rules.maxLength(60)]),
-        sobrenome: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(3),
-          rules.maxLength(60)]),
-        fone: schema.string({trim: true}, [
-          rules.required(),
-          rules.minLength(10),
-          rules.maxLength(20)]),
-        email: schema.string({trim: true}, [
-          rules.required(),
-          rules.unique({table: 'usuarios', column: 'email'}),
-          rules.unique({table: 'clientes', column: 'email'}),
-          rules.email]),
-        password: schema.string({trim: true}, [
-            rules.required(),
-            rules.minLength(6),
-            rules.maxLength(64)]),
-      })
+  public async index() {
+    const usuario = await Usuario.all()
 
-      const messages = {
-        minLength: '{{ field }} deve ter no mínimo {{ options.minLength }} caracteres',
-        maxLength: '{{ field }} deve ter no máximo {{ options.maxLength }} caracteres',
-        required: '{{ field }} é obrigatório',
-        unique: '{{ field }} deve ser único, esse {{ field }} já foi utilizado',
-        regex: '{{ field}} deve seguir o padrão: xxx.xxx.xxx-xx onde x = número',
-        number:'{{ field }} deve conter um número',
-        exists: '{{ field }} deve existir na tabela {{ options.table }}'
-      }
-
-
-      const body = await request.validate({schema: validationSchema, messages})
-      
-        const usuario = await Usuario.findOrFail(params.id)
-    
-        usuario.nome = body.nome
-        usuario.sobrenome = body.sobrenome
-        usuario.fone = body.fone
-        usuario.email = body.email
-        usuario.password = body.password
-    
-        
-        await usuario.save()
-
-        
-    
-        return{
-          message: 'usuario Atualizado com Sucesso!',
-          data: usuario,
-        }
+    return {
+      data: usuario,
     }
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const usuario = await Usuario.findOrFail(params.id)
+
+    return {
+      data: usuario,
+    }
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const usuario = await Usuario.findOrFail(params.id)
+    await usuario.delete()
+
+    return {
+      message: 'usuario Deletado com sucesso!',
+      data: usuario,
+    }
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const validationSchema = schema.create({
+      nome: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(3),
+        rules.maxLength(60),
+      ]),
+      sobrenome: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(3),
+        rules.maxLength(60),
+      ]),
+      fone: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(10),
+        rules.maxLength(20),
+      ]),
+      email: schema.string({ trim: true }, [
+        rules.required(),
+        rules.unique({ table: 'usuarios', column: 'email' }),
+        rules.unique({ table: 'clientes', column: 'email' }),
+        rules.email(),
+      ]),
+      password: schema.string({ trim: true }, [
+        rules.required(),
+        rules.minLength(6),
+        rules.maxLength(64),
+      ]),
+    })
+
+    const messages = {
+      minLength: '{{ field }} deve ter no mínimo {{ options.minLength }} caracteres',
+      maxLength: '{{ field }} deve ter no máximo {{ options.maxLength }} caracteres',
+      required: '{{ field }} é obrigatório',
+      unique: '{{ field }} deve ser único, esse {{ field }} já foi utilizado',
+      regex: '{{ field}} deve seguir o padrão: xxx.xxx.xxx-xx onde x = número',
+      number: '{{ field }} deve conter um número',
+      exists: '{{ field }} deve existir na tabela {{ options.table }}',
+    }
+
+    const body = await request.validate({ schema: validationSchema, messages })
+
+    const usuario = await Usuario.findOrFail(params.id)
+
+    usuario.nome = body.nome
+    usuario.sobrenome = body.sobrenome
+    usuario.fone = body.fone
+    usuario.email = body.email
+    usuario.password = body.password
+
+    await usuario.save()
+
+    return {
+      message: 'usuario Atualizado com Sucesso!',
+      data: usuario,
+    }
+  }
 }
