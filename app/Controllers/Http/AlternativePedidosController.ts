e582550c5c0cb75e8b54137de3c9b8c79175f373
febomtempo <febomtempo@hotmail.com>
2022-06-10 20:33:57 -0300

    correção alternativePedido

--- app/Controllers/Http/AlternativePedidosController.ts
+++ app/Controllers/Http/AlternativePedidosController.ts
@@ -3,7 +3,11 @@ import Pedido from 'App/Models/Pedido'
 
 export default class AlternativePedidosController {
   public async show({ params }: HttpContextContract) {
-    const pedido = await Pedido.query().where('id_cliente', params.id)
+    const pedido = await Pedido.query()
+      .where('id_cliente', params.id)
+      .preload('produtos')
+      .preload('clientes')
+      .preload('endereco')
 
     return {
       data: pedido,
