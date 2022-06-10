86eac2647a7820ca9798a0db01f8e8cc98a01ff8
febomtempo <febomtempo@hotmail.com>
2022-06-10 15:22:54 -0300

    correção endereco alternativo

--- app/Controllers/Http/AlternativeEnderecosController.ts
+++ app/Controllers/Http/AlternativeEnderecosController.ts
@@ -3,7 +3,7 @@ import Endereco from 'App/Models/Endereco'
 
 export default class AlternativeEnderecosController {
   public async show({ params }: HttpContextContract) {
-    const endereco = await Endereco.findByOrFail('id_cliente', params.id)
+    const endereco = await Endereco.query().where('id_cliente', params.id)
     return {
       data: endereco,
     }
