c9760a57c74617937ff40676ba3e804632fbe8b3
febomtempo <febomtempo@hotmail.com>
2022-06-10 15:18:05 -0300

    .

--- app/Controllers/Http/AlternativeEnderecosController.ts
+++ app/Controllers/Http/AlternativeEnderecosController.ts
@@ -1,7 +1,7 @@
 import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import Endereco from 'App/Models/Endereco'
 
-export default class EnderecosController {
+export default class AlternativeEnderecosController {
   public async show({ params }: HttpContextContract) {
     const endereco = await Endereco.findByOrFail('id_cliente', params.id)
     return {
